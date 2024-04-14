241e73192a827d60b00da9eb52437cdf423122a8
minsoo <minsoo651@naver.com>
2024-04-14 23:17:04 +0900

    Feat: Querydsl 설정 추가

---
+++ src/main/java/com/yh/erp/infrastructure/querydsl/QuerydslConfig.java
@@ -0,0 +1,20 @@
+package com.yh.erp.infrastructure.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+
+}
